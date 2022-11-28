afd122414b3f757e6c04929d16012cf2ef9698b0
Viktoriia Semeniuk <zakharuk.vita@gmail.com>
2022-11-29 00:29:59 +0200

    adds more code

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
-
+import Notiflix from "notiflix";
 function fetchCountries(name) {
   return fetch(
-    `&{BASE_URL}&{name}?fields=name,capital,populatin,flags.svg,languages"`
+    `${BASE_URL}${name}?fields=name,capital,population,flags,languages"`
   ).then(r => {
     if (!r.ok) {
        throw new Error(Notiflix.Notify.failure("Oops, there is no country with that name")) 
